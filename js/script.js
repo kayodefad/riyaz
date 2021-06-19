35fbf69dbf232956a79efddcb567207d290db5c3
Oluwakayode Fadoju <fadojuk@gmail.com>
2021-06-20 00:15:49 +0100

    Multitired

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -31,51 +29,61 @@ window.onclick = function (event) {
 //   all ------------------
 function initParadoxWay() {
   "use strict";
- 
+
   if ($(".testimonials-carousel").length > 0) {
-      var j2 = new Swiper(".testimonials-carousel .swiper-container", {
-          preloadImages: false,
-          slidesPerView: 1,
-          spaceBetween: 20,
-          loop: true,
-          grabCursor: true,
-          mousewheel: false,
-          centeredSlides: true,
-          pagination: {
-              el: '.tc-pagination',
-              clickable: true,
-              dynamicBullets: true,
-          },
-          navigation: {
-              nextEl: '.listing-carousel-button-next',
-              prevEl: '.listing-carousel-button-prev',
-          },
-          breakpoints: {
-              1024: {
-                  slidesPerView: 3,
-              },
-              
-          }
-      });
+    var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+      preloadImages: false,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      mousewheel: false,
+      centeredSlides: true,
+      pagination: {
+        el: ".tc-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+      navigation: {
+        nextEl: ".listing-carousel-button-next",
+        prevEl: ".listing-carousel-button-prev",
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
   }
-  
-// bubbles -----------------
-  
-  
+
+  // bubbles -----------------
+
   setInterval(function () {
-      var size = randomValue(sArray);
-      $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
-      $('.individual-bubble').animate({
-          'bottom': '100%',
-          'opacity': '-=0.7'
-      }, 4000, function () {
-          $(this).remove()
-      });
+    var size = randomValue(sArray);
+    $(".bubbles").append(
+      '<div class="individual-bubble" style="left: ' +
+        randomValue(bArray) +
+        "px; width: " +
+        size +
+        "px; height:" +
+        size +
+        'px;"></div>'
+    );
+    $(".individual-bubble").animate(
+      {
+        bottom: "100%",
+        opacity: "-=0.7",
+      },
+      4000,
+      function () {
+        $(this).remove();
+      }
+    );
   }, 350);
-  
 }
 
 //   Init All ------------------
 $(document).ready(function () {
   initParadoxWay();
 });
+
