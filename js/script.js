e8629c80b7509160446828f734da06f2b3d60203
Oluwakayode Fadoju <fadojuk@gmail.com>
2021-06-27 00:13:56 +0100

    Boo

--- js/script.js
+++ js/script.js
@@ -89,12 +89,23 @@ $(document).ready(function () {
 
 const gallery_col = document.querySelectorAll(".column");
 
-// gallery_col[0].children[0].style.height = "400px";
+// scroll to top
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-// for (let i = 0; i < gallery_col.length; i++) {
-//   let gal_child = gallery_col[i].children;
-//   for (let j = 0; j < gal_child.length; j++) {
-//     if (j % 2 == 0) gal_child[j].style.height = "200px";
-//     else gal_child[j].style.height = "300px";
-//   }
-// }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
